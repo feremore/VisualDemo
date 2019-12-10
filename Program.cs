74d1121dc8e0dfaa24e94c37dae79bfe85d4339a
Kyle Loewenstein <kloewenmore@outlook.com>
2019-12-10 13:55:26 -0600

    pushing code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
